d9d46013e282efe5cc28c7c8dbc69e47644a1b15
Jay Lee <dreampoetlee@gmail.com>
2022-04-06 15:07:06 -0400

    Completed the following tasks: - Changed the name of previous listed files - renamed: lizard.py --> player.py - renamed: paper.py --> computer.py - renamed: rock.py --> human.py - renamed: scissors.py to game.py - deleted spock.py

--- algorithm.py
+++ algorithm.py
@@ -38,3 +38,22 @@
 # As a player, I want the correct player to win a given round based on the choices* made by each player.
 
 # As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary.
+
+# ^ Logic thoughts from Tarsha
+  # 1 MAIN-  import Game. game_run
+  # 2 AI/PC- pc where  will be importing random options.
+  # 3 Human file-can choose their option to play game
+  # 4 Game file-Runs entire game. import AI, import human, import options, import rules. display welcome, random choice options for player.
+
+  #! Classes needed 
+    #todo Parent class
+    #? Player
+      # has a: 
+      # - name
+      # - list of possible gestures
+      # - chosen_gesture
+      # - score
+    #& Child classes
+
+
+    # renamed the following files: lizard to player...paper to AI....rock to human...scissors to game...deleted spock ile
--- spock.py
+++
@@ -1,7 +0,0 @@
-# imports here
-
-#! Game rules for Spock
-  #todo Lizard poisons Spock
-  #todo Spock smashes Scissors
-  #todo Paper disproves Spock 
-  #todo Spock vaporizes Rock
