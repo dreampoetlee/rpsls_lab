1c76ec723957fe3050decd692bd1a6d1e1318c42
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-04-06 13:36:13 -0600

    Cloned a copy of the repo to my computer and made the following changes: - completed the main.py file and the main class - started working on the player class and player.py file

--- human.py
+++
@@ -1,7 +0,0 @@
-# imports here
-
-#! Game rules for Rock
-  #todo Rock crushes Scissors 
-  #todo Paper covers Rock
-  #todo Rock crushes Lizard
-  #todo Spock vaporizes Rock
--- player.py
+++ player.py
@@ -1,7 +1,2 @@
-# imports here
-
-#! Game rules for Rock
-  #todo Scissors decapitates Lizard 
-  #todo Lizard eats Paper
-  #todo Rock crushes Lizard
-  #todo Lizard poisons Spock 
+class Player:
+    def __init__(self):
