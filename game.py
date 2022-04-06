5328deee615d28348bf94783eed570098b0a664c
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-04-06 15:44:43 -0600

    defined player one, defined player 2, tuped out welcome message for display, displayed rules to print in terminal

--- game.py
+++ game.py
@@ -3,7 +3,23 @@ from computer import Computer
 from player import Player
 
 
-
 class Game:
     def __init__(self):
-        pass
+        self.player_one = Human
+        self.player_two = Player
+
+    def display_welcome_message(self):
+        print("Welcome to Rock,Paper,Scissors,Lizard,Spock!!!")
+
+    def display_rules(self):
+        print("Here are the rules for the Game!")
+        print("Rock crushes Scissors!!")
+        print("Scissors cuts Paper, Paper covers Rock!")
+        print("Rock crushes Lizard")
+        print("But Lizard poisons Spock")
+        print("Spock smashes Scissors")
+        print("Scissors decapitates Lizard. Brutal!")
+        print("Lizard eats Paper, Nom!")
+        print("Paper disproves Spock")
+        print("And Spock vaporizes Rock!")
+    
