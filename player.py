c4c6b1a3200629019ff6cf4b1dce384269bbc360
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-04-06 14:22:23 -0600

    added gestures to player.py file, defined self, player 1 and 2, working on chosen gestures in player.py file

--- player.py
+++ player.py
@@ -1,2 +1,10 @@
 class Player:
     def __init__(self):
+        self.name = ""
+        self.gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture = []
+        self.player_one = None
+        self.player_two = None
+    
+    def chosen_gestures(self):
+        print("Type O for Rock")
