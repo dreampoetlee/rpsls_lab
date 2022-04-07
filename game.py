b888f147fc7912553710c00341a7b42e3b300503
Jay Lee <dreampoetlee@gmail.com>
2022-04-07 12:06:51 -0400

    Completed the following tasks: - Added comments to the human and computer file trying to work out the what needs to be done - Added similar comments to the game.py file

--- game.py
+++ game.py
@@ -7,3 +7,22 @@ from player import Player
 class Game:
     def __init__(self):
         pass
+
+    def run_game(self):
+        self.display_welcome()
+        self.gameplay()
+        self.display_winner()
+        pass
+
+    def display_welcome(self):
+        #todo print welcome message
+        #todo print the rules of the game
+        
+        pass
+
+    def gameplay(self):
+        #todo need a while loop to run till 
+        pass
+
+    def display_winner(self):
+        pass
--- human.py
+++ human.py
@@ -1,6 +1,7 @@
 from player import Player
 
 class Human(Player):
-  def __init__(self):
-      pass
+  def __init__(self, name):
+    #! player can choose a gesture, but is it inherited from the parent class or does it have to be placed here
+      super().__init__(name)
 
